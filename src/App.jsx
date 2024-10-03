82bfeff9a580168b8327b29221b89f7e634b080a
robiulsagor <robiul100.me@gmail.com>
2024-10-03 20:04:41 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <Button variant="destructive">Shadcn</Button>
+    </div>
+  )
+}
+
+export default App
